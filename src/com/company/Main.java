f6d6bffcbb08b06dfc0581d84689301a49e86daa
MRiickyy <2200018191@webmail.uad.ac.id>
2023-12-30 23:05:35 +0700

    Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,34 @@
 package com.company;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hallo World!");
+// Kelas untuk mengelola saldo
+class EWallet {
+    private double saldo;
+
+    public EWallet() {
+        this.saldo = 0;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void topUp(double amount) {
+        // Simulasi penambahan saldo
+        saldo += amount;
+        System.out.println("Top-up berhasil. Saldo sekarang: " + saldo);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        EWallet eWallet = new EWallet();
+
+        // Simulasi top-up
+        double topUpAmount = 50.0;
+        eWallet.topUp(topUpAmount);
+
+        // Simulasi menampilkan saldo
+        System.out.println("Saldo sekarang: " + eWallet.getSaldo());
+    }
+}
+
